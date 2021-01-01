ec9796eb1a319c9df3e4542da07176082439ea68
Harshit Singhal <33669026+123Harshit@users.noreply.github.com>
2021-01-02 00:19:49 +0530

    added header

--- src/Header.js
+++ src/Header.js
@@ -6,6 +6,7 @@ import HomeIcon from "@material-ui/icons/Home"
 import SupervisorAccountIcon from '@material-ui/icons/SupervisorAccount'
 import BusinessCenterIcon from '@material-ui/icons/BusinessCenter'
 import ChatIcon from '@material-ui/icons/Chat'
+import NotificationIcon from '@material-ui/icons/Notifications'
 
 function Header() {
     return (
@@ -20,8 +21,10 @@ function Header() {
             <div className="header_right">
                 <HeaderOption Icon={HomeIcon} title="Home"/>
                 <HeaderOption Icon={SupervisorAccountIcon} title="My Network"/>
-                <HeaderOption Icon={BusinessCenterIcon} title="Business"/>
+                <HeaderOption Icon={BusinessCenterIcon} title="Jobs"/>
                 <HeaderOption Icon={ChatIcon} title="Messaging"/>
+                <HeaderOption Icon={NotificationIcon} title="Notification"/>
+                <HeaderOption avatar="https://media-exp1.licdn.com/dms/image/C5103AQETbyJ8CWEhYA/profile-displayphoto-shrink_200_200/0/1575055818407?e=1614211200&v=beta&t=8kfaHosm4tbwrZ2FJlaeyPY4jqzZ8DIEzGCXFsi5SR8" title="Me"/>
             </div>
         </div>
     )
